d0c9162c430486205c87e0f618e2c92751247f12
Petter Sæther Moen <pettersmoen@gmail.com>
2025-05-18 20:07:46 +0200

    replace refetching logic with revalidatePath

--- app/move-card-action.ts
+++ app/move-card-action.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import { moveCard, type MoveEvent } from "./backend-stuff/move-card";
+import { revalidatePath } from "next/cache";
 
 export async function moveCardAction({ cardId, moveTo }: MoveEvent) {
   await moveCard(cardId, moveTo);
+  revalidatePath("/");
 }
