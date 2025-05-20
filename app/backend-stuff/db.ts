fd32507b93125c0011ddbfbf0e34539c0a483abb
Petter Sæther Moen <pettersmoen@gmail.com>
2025-05-20 13:35:21 +0200

    stop backend functions from being used in client code

--- app/backend-stuff/db.ts
+++ app/backend-stuff/db.ts
@@ -1,3 +1,5 @@
+import "server-only";
+
 import { promises as fs } from "fs";
 import path from "path";
 
--- app/backend-stuff/move-card.ts
+++ app/backend-stuff/move-card.ts
@@ -1,14 +1,15 @@
 // The hidden backend for moving cards on the board.
 // This code is meant to simulate the slowness and "randomness" of real-world APIs.
 
+import "server-only";
+
 import { db } from "./db";
 
 let moveCount = 0;
 export async function moveCard(id: string, to: number): Promise<number> {
   await new Promise((resolve) =>
-    setTimeout(resolve, moveCount % 2 === 0 ? 2000 : 6000)
+    setTimeout(resolve, moveCount++ % 2 === 0 ? 1000 : 3000)
   );
-  moveCount++;
   await db.update(id, { column: to });
   return to;
 }
